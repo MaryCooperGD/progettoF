70cde71879fb7d905eb0fe1a89f7fdb12ab97b56
MaryCooperGD <marycoopergd@gmail.com>
2019-02-13 11:38:05 +0100

    pagina filtri

--- src/app/choosefilter/choosefilter.page.ts
+++ src/app/choosefilter/choosefilter.page.ts
@@ -8,16 +8,60 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ChoosefilterPage implements OnInit {
 
+  city: string;
   title: any;
+  selectedArray: any=[];
 
+  public form = [
+        { val: 'Carte di credito', isChecked: false },
+        { val: 'Prenotazioni', isChecked: false },
+        { val: 'Servizio a domicilio', isChecked: false },
+        { val: 'Servizio di asporto', isChecked: false },
+        { val: 'Wi-Fi', isChecked: false },
+        { val: 'Posti all\'aperto', isChecked: false },
+        { val: 'Aperto da poco', isChecked: false },
+        { val: 'Non parte di una catena', isChecked: false },
+        { val: 'Prenotazioni online', isChecked: false },
+        { val: 'Accetta animali', isChecked: false },
+        { val: 'Dotato di parcheggio', isChecked: false }
+      ];
+
+      public days = [
+        {val: 'Lunedì', isChecked: false},
+        {val: 'Martedì', isChecked: false},
+        {val: 'Mercoledì', isChecked: false},
+        {val: 'Giovedì', isChecked: false},
+        {val: 'Venerdì', isChecked: false},
+        {val: 'Sabato', isChecked: false},
+        {val: 'Domenica', isChecked: false}
+      ];
   constructor(private route: ActivatedRoute) { }
 
 
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-       this.title = params['id'];
+       this.title = "Cerca un posto per " + params['id'].toLowerCase();
   });
+  }
+
+  charMemberSelected(entry){
+    entry.isChecked = !entry.isChecked;
+    if(entry.isChecked){
+      this.selectedArray.push(entry);
+    } else {
+      let newArray = this.selectedArray.filter(function(el){
+        return el.val !==  entry.val;
+      });
+      this.selectedArray = newArray;
+    }
+    console.log(entry.val);
+  }
+
+
+  searchPlaces(){
+    //costruzione query
+
   }
 
 }
